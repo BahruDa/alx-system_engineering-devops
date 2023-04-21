82ffdb235bb254622f2e4cf4dd6db792c4d0ff4d
BahruDa <bahrudari68@gmail.com>
2023-04-21 19:24:05 +0300

    .

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
-# Using Puppet, create a file in /tmp.
-file { '/tmp/holberton': #the path of the new file
-  ensure  => 'present',
-  path    => '/tmp/holberton',
+# creates a file in "/tmp" directory
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  mode    => '0744',
-  content => 'I love Puppet';
+  content => 'I love Puppet',
 }
