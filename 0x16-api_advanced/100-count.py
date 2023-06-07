8dee1fb73af2881854020d64aedcad10895473e4
BahruDa <bahrudari68@gmail.com>
2023-06-07 22:05:44 +0300

    fixed

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,55 +1,56 @@
 #!/usr/bin/python3
-"""Function to count words in all hot posts of a given Reddit subreddit."""
-import requests
+"""
+Traceback function to verify that a student's function is recursive.
+"""
+import sys
+import traceback
+
+from contextlib import redirect_stdout
+from io import StringIO
 
 
-def count_words(subreddit, word_list, instances={}, after="", count=0):
-    """Prints counts of given words found in hot posts of a given subreddit.
-    Args:
-        subreddit (str): The subreddit to search.
-        word_list (list): The list of words to search for in post titles.
-        instances (obj): Key/value pairs of words/counts.
-        after (str): The parameter for the next page of the API results.
-        count (int): The parameter of results matched thus far.
+def check_recursion():
     """
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    try:
-        results = response.json()
-        if response.status_code == 404:
-            raise Exception
-    except Exception:
-        print("")
-        return
+    Verifies that a student's code is using recursion.
+    """
+    sys.settrace(trace_calls)
+    null = StringIO()
+    trace = StringIO()
+    stderr = sys.stderr
+    sys.stderr = trace
 
-    results = results.get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        title = c.get("data").get("title").lower().split()
-        for word in word_list:
-            if word.lower() in title:
-                times = len([t for t in title if t == word.lower()])
-                if instances.get(word) is None:
-                    instances[word] = times
-                else:
-                    instances[word] += times
+    with redirect_stdout(null):
+        count_words('hello', ['hello'])  # r/hello is an existing subreddit which almost certainly contains 'hello'
+    sys.stderr = stderr              # in its posts
 
-    if after is None:
-        if len(instances) == 0:
-            print("")
-            return
-        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
-        [print("{}: {}".format(k, v)) for k, v in instances]
+    if len(trace.getvalue()) > 0:
+        print("function is recursive", end="")
     else:
-        count_words(subreddit, word_list, instances, after, count)
-        
+        print("function is not recursive", end="")
+
+
+def trace_calls(frame, event, arg):
+    if event != 'call': # We only want function calls!
+        return
+    co = frame.f_code
+    func_name = co.co_name
+    if func_name != 'count_words': # We only want function calls of THIS function
+        return
+    func_line_no = frame.f_lineno
+    func_filename = co.co_filename
+    caller = frame.f_back
+    caller_line_no = caller.f_lineno
+    caller_filename = caller.f_code.co_filename
+    if 'main' in caller_filename: # Ignore any calls made in the main
+        return
+    sys.stderr.write('Call to {} on line {} of {} from line {} of {}\n'
+                     .format(func_name,
+                             func_line_no,
+                             func_filename,
+                             caller_line_no,
+                             caller_filename))
+
+
+if __name__ == "__main__":
+    count_words = __import__("100-count").count_words
+    check_recursion() 
