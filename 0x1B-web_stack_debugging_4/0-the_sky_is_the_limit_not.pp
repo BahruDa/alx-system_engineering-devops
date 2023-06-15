02274b42d9d842000bf39adbd193828b9092c609
BahruDa <bahrudari68@gmail.com>
2023-06-15 21:00:54 +0300

    fixed

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,11 @@
-# Increases the amount of traffic an Nginx server can handle.
+# puppet file to debug nginx server
 
-# Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
+exec { 'sed -i "s/15/4096/" /etc/default/nginx':
+  cwd  => '/root',
+  path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
+}
 
-# Restart Nginx
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+exec { 'sudo service nginx restart':
+  cwd  => '/root',
+  path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
 }
